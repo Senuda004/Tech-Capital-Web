137499c6e8e76f41353c1f3a531c54c4d3563780
Senuda004 <senudamusic120@gmail.com>
2024-04-26 19:52:39 +0530

    Update FAQItem component styles and faqData content

--- components/FAQ/FAQItem.tsx
+++ components/FAQ/FAQItem.tsx
@@ -16,7 +16,7 @@ const FAQItem = ({ faqData }: { faqData: FaqData }) => {
           onClick={() => {
             handleFaqToggle(id);
           }}
-          className="flex cursor-pointer items-center justify-between px-6 py-5 text-metatitle3 font-medium text-black dark:text-white lg:px-9 lg:py-7.5"
+          className="flex cursor-pointer items-center justify-between px-6 py-5 text-metatitle4 font-medium text-black dark:text-white lg:px-9 lg:py-7.5 text-left leading-8"
         >
           {quest}
 
@@ -49,7 +49,7 @@ const FAQItem = ({ faqData }: { faqData: FaqData }) => {
           )}
         </button>
         <p
-          className={`border-t border-stroke px-6 py-5 dark:border-strokedark lg:px-9 lg:py-7.5 ${
+          className={`border-t text-justify border-stroke px-6 py-5 dark:border-strokedark lg:px-9 lg:py-7.5 ${
             activeFaq === id ? "block" : "hidden"
           }`}
         >
--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,18 +3,18 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "What sets Tech Capital Managed Services apart from other IT service providers?",
+    ans: "Tech Capital Managed Services excels in delivering personalized IT solutions that align closely with our clients' business goals. Our commitment to reliability, customer satisfaction, and continuous improvement ensures that we provide superior service compared to other providers.",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How does Tech Capital ensure the security of its managed IT services?",
+    ans: "We employ state-of-the-art security technologies and practices, including regular audits, compliance checks, and proactive threat detection to ensure the utmost security and integrity of our clients’ data and IT infrastructure.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Can Tech Capital Managed Services help my business scale its operations?",
+    ans: "Absolutely. Our resource outsourcing and infrastructure management services are designed to be scalable to meet the growing demands of your business, helping you expand seamlessly and efficiently.",
   },
 ];
 
